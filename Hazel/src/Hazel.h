37d0e3768d8f507a64f5fc09c69f2d781b328e0d
Swaroop PrakashKumar <Swaroop.PrakashKumar@analog.com>
2024-04-26 22:38:08 +0530

    Set basic Hazel Application

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For include in Hazel applications
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// -- Entry Point -----------------------------
+#include "Hazel/EntryPoint.h"
+// --------------------------------------------
